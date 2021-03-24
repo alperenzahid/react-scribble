0d798e037447dd3c3f845cfaf2c3dc1bb529556f
alperenzahid <biz@linspark.com>
2021-03-24 09:11:18 +0300

    - menu_item extracted as a constant - main logo size is increased

--- components/nav/navigation.jsx
+++ components/nav/navigation.jsx
@@ -1,32 +1,25 @@
 import React from 'react'
+
 import NavigationButton from '../buttons/navigation-button'
 import TextTitle from '../text/text-title'
-import * as menuItems from '../icons/menu'
-let { Twitter } = menuItems
-let navMenuItems = [
-  { key: 'Home', notify: 0, selectedIcon: 'HomeFill' },
-  { key: 'Explore', notify: false, selectedIcon: 'ExploreFill' },
-  { key: 'Notification', notify: 3, selectedIcon: 'NotificationFill' },
-  { key: 'Messages', notify: 44, selectedIcon: 'MessagesFill' },
-  { key: 'Bookmark', notify: 6, selectedIcon: 'BookmarkFill' },
-  { key: 'Lists', selectedIcon: 'ListsFill' },
-  { key: 'Profile', notify: 1, selectedIcon: 'ProfileFill' },
-  { key: 'More', selectedIcon: 'More' }
-]
+import * as menuIconItems from '../icons/menu'
+let { Twitter } = menuIconItems
+import { NAV_MENU_ITEM } from '../../constants'
+
 import styles from './navigation.module.css'
 
 function Navigation({ selectedKey = 'home', flat = false }) {
   return (
     <nav className={styles.nav}>
       <NavigationButton>
-        <Twitter />
+        <Twitter style={{ fontSize: 30 }} />
       </NavigationButton>
 
-      {navMenuItems.map((item) => {
+      {NAV_MENU_ITEM.map((item) => {
         let _selected = selectedKey.toLowerCase() === item.key.toLowerCase()
         let MenuItem = _selected
-          ? menuItems[item.selectedIcon]
-          : menuItems[item.key]
+          ? menuIconItems[item.selectedIcon]
+          : menuIconItems[item.key]
         return (
           <NavigationButton
             key={item.key}
--- constants/index.js
+++ constants/index.js
@@ -2,3 +2,14 @@ export default {
   TABLET_SIZE: 980,
   DESKTOP_SIZE: 1270
 }
+
+export const NAV_MENU_ITEM = [
+  { key: 'Home', notify: 0, selectedIcon: 'HomeFill' },
+  { key: 'Explore', notify: false, selectedIcon: 'ExploreFill' },
+  { key: 'Notification', notify: 3, selectedIcon: 'NotificationFill' },
+  { key: 'Messages', notify: 44, selectedIcon: 'MessagesFill' },
+  { key: 'Bookmark', notify: 6, selectedIcon: 'BookmarkFill' },
+  { key: 'Lists', selectedIcon: 'ListsFill' },
+  { key: 'Profile', notify: 1, selectedIcon: 'ProfileFill' },
+  { key: 'More', selectedIcon: 'More' }
+]
